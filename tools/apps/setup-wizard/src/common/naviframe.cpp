58e318d9dddb868b9842d2988abd81330c014b06
Jaemin Ryu <jm77.ryu@samsung.com>
2016-07-20 10:15:56 +0900

    [WIP] Rework setup-wizard
    Change-Id: I49699d15bc22ccb84104620813971d03b9ff9a4b
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ tools/apps/setup-wizard/src/common/naviframe.cpp
@@ -0,0 +1,46 @@
+#include "naviframe.h"
+
+NaviItem::NaviItem(Elm_Object_Item* object) :
+	item(object)
+{
+}
+
+Eina_Bool NaviItem::eventHandlerDispatcher(void *data, Elm_Object_Item* it)
+{
+	NaviItemEventListener* listener = reinterpret_cast<NaviItemEventListener *>(data);
+	return listener->handleNaviItemEvent();
+}
+
+NaviFrame::NaviFrame(Widget* parent)
+{
+	nativeObject = elm_naviframe_add(Widget::object(parent));
+	eext_object_event_callback_add(nativeObject, EEXT_CALLBACK_BACK, eext_naviframe_back_cb, NULL);
+}
+
+NaviFrame::~NaviFrame()
+{
+}
+
+NaviItem NaviFrame::push(Widget* content)
+{
+	NaviItem item(elm_naviframe_item_push(nativeObject, NULL, NULL, NULL, Widget::object(content), NULL));
+	//elm_naviframe_content_preserve_on_pop_set(Widget::object(content), true);
+	item.setTitleEnabled(false, true);
+	evas_object_ref(Widget::object(content));
+	return item;
+}
+
+NaviItem NaviFrame::push(Widget* content, const std::string& title)
+{
+	NaviItem item(elm_naviframe_item_push(nativeObject, title.c_str(), NULL, NULL, Widget::object(content), NULL));
+	//elm_naviframe_content_preserve_on_pop_set(Widget::object(content), true);
+	item.setTitleEnabled(true, true);
+	evas_object_ref(Widget::object(content));
+
+	return item;
+}
+
+void NaviFrame::pop()
+{
+	elm_naviframe_item_pop(nativeObject);
+}
